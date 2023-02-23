69a9475906c1b2ea8f86e5e25de01e024532380d
Vijay Pandian <ananth.vijay@gmail.com>
2023-02-23 10:03:20 -0500

    Update versions.tf

--- nomad-gcp/versions.tf
+++ nomad-gcp/versions.tf
@@ -1,9 +1,4 @@
 terraform {
-  # Specify backend here
-  backend "gcs" {
-    bucket  = "tf-state-nomad-vijay"
-    prefix  = "terraform/state"
-  }
   required_providers {
     google = {
       version = "~> 3.0"
