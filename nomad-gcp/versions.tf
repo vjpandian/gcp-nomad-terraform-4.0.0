ec78994572bda78ff79f8b949be61ea9b7a7c7b8
Vijay Pandian <ananth.vijay@gmail.com>
2023-02-23 09:48:35 -0500

    specify backend bucket

--- nomad-gcp/versions.tf
+++ nomad-gcp/versions.tf
@@ -2,9 +2,14 @@ terraform {
   required_providers {
     google = {
       version = "~> 3.0"
-      // NOTE: This can be set to hashicorp/google once scaling_schedules are
-      // out of beta on google_compute_autoscaler resources
+      # NOTE: This can be set to hashicorp/google once scaling_schedules are
+      # out of beta on google_compute_autoscaler resources
       source = "hashicorp/google-beta"
     }
+  }
+  # Specify backend here
+  backend "gcs" {
+    bucket  = "tf-state-nomad-vijay"
+    prefix  = "terraform/state"
   }
 }
