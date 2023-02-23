8a18770c111ec63a80f05ffeaa4dbceb0e3bfe81
Vijay Pandian <ananth.vijay@gmail.com>
2023-02-22 21:42:11 -0500

    pushing modified config

---
+++ nomad.tf
@@ -0,0 +1,19 @@
+provider "google-beta" {
+  project = "dev-vijay-pandian"
+  region  = "northamerica-northeast2"
+  zone    = "northamerica-northeast2-a"
+}
+
+module "nomad" {
+  # We strongly recommend pinning the version using ref=<<release tag>> as is done here
+  source = "git::https://github.com/vjpandian/gcp-nomad-terraform-4.0.0.git//nomad-gcp?ref=main"
+
+  zone            = "northamerica-northeast2-a"
+  region          = "northamerica-northeast2"
+  network         = "default"
+  server_endpoint = "cci4.vijaypandian.xyz:4647"
+}
+
+output "module" {
+  value = module.nomad
+}
